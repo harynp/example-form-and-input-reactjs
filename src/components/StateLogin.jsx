82481ff24f9d29ee66b42eefe28b419d43343c4a
harynp <harynugrahaputra@gmail.com>
2024-06-25 00:48:22 +0700

    learn the best way for form input with useState, useRef, custom hook, and conjuction function browser

---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import Input from "./Input";
+import {
+  isEmail,
+  hasMinLength,
+  isEqualsToOtherValue,
+  isNotEmpty,
+} from "../util/validation";
+import { useInput } from "../hooks/useInput";
+
+// with useState()
+export default function Login() {
+  const {
+    value: emailValue,
+    handleInputBlur: handleEmailBlur,
+    handleInputChange: handleEmailChange,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
+
+  const {
+    value: passwordValue,
+    handleInputBlur: handlePasswordBlur,
+    handleInputChange: handlePasswordChange,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 6));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (emailHasError && passwordHasError) {
+      return;
+    }
+
+    console.log(emailValue, passwordValue);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div className="control-row">
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "Please enter a valid email !"}
+        />
+
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && "Please enter a valid password !"}
+        />
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
